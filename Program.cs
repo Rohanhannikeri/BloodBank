b29755455e9e4523982b1de9e7b70ec8f89982a4
Rohanhannikeri <91936077+Rohanhannikeri@users.noreply.github.com>
2022-06-01 11:09:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using CRUDDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContextPool<DonationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ServerConn"));
+
+});
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
